ecaf506de4ca2a43906ad4b4832a8f13fc04ca5c
Mohammadhossein Salari <mohammadhossein.salari@gmail.com>
2025-07-25 13:02:04 +0300

    chore: remove old eye_trackers directory after refactoring

--- eye_trackers/hennessey/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Hennessey et al. geometric eye tracking implementation.
-
-This module implements the geometric eye tracking method described in
-Hennessey et al. papers, which uses corneal curvature estimation and
-geometric relationships between pupil center and corneal reflections
-to estimate gaze direction.
-"""
-
-from .hennessey_tracker import HennesseyTracker
--- eye_trackers/interpolate/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Polynomial interpolation-based eye tracking implementation.
-
-This module implements eye tracking using polynomial interpolation methods
-for mapping pupil-corneal reflection vectors to gaze coordinates.
-"""
-
-from .interpolation_tracker import InterpolationTracker
