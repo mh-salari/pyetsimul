8ac0c83b59c7c02ee7d72378cfcaad02ea595f4b
Mohammadhossein Salari <mohammadhossein.salari@gmail.com>
2026-09-10 10:30:32 +0300

    chore: widen the docs content column

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,8 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "navigation_depth": 4,
 }
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # General
 exclude_patterns = ["_build"]
